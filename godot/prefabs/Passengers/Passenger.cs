7309466df7a66fdeaaf4b332dfb2240a3fa8aec4
Ren <renimations@gmail.com>
2023-08-02 04:43:51 -0500

    Material, function and idea updates

--- godot/prefabs/Passengers/Passenger.cs
+++ godot/prefabs/Passengers/Passenger.cs
@@ -4,31 +4,31 @@ using Godot;
 
 public partial class Passenger : Area3D
 {
-    [Export]
-    public float MaxDeliveryScore { get; set; }
+	[Export]
+	public float MaxDeliveryScore { get; set; }
 
-    /// <summary>
-    /// This is the time to get half the MaxDeliveryScore, double this time = 0 points
-    /// </summary>
-    [Export]
-    public float DeliveryParTimeSeconds { get; set; }
+	/// <summary>
+	/// This is the time to get half the MaxDeliveryScore, double this time = 0 points
+	/// </summary>
+	[Export]
+	public float DeliveryParTimeSeconds { get; set; }
 
-    [Export]
-    public Target Target { get; set; }
+	[Export]
+	public Target Target { get; set; }
 
-    [Signal]
-    public delegate void PassengerDeliveredEventHandler();
+	[Signal]
+	public delegate void PassengerDeliveredEventHandler();
 
-    public Passenger()
-    {
-        BodyEntered += Passenger_BodyEntered;
-    }
+	public Passenger()
+	{
+		BodyEntered += Passenger_BodyEntered;
+	}
 
-    private void Passenger_BodyEntered(Node3D body)
-    {
-        if (body is Turtle t)
-        {
-            t.Transport.TryPickupPassenger(this);
-        }
-    }
+	private void Passenger_BodyEntered(Node3D body)
+	{
+		if (body is Turtle t)
+		{
+			t.Transport.TryPickupPassenger(this);
+		}
+	}
 }
